be91a2c82f72684052e40597367ecc3f8f85099c
Mustafa YILDIRIM <mustafa.mym9@gmail.com>
2021-10-20 14:22:06 +0300

    solve meltdown-mitigation

---
+++ meltdown-mitigation/conditionals.py
@@ -0,0 +1,77 @@
+""" Meltdown Mitigation exercise """
+
+
+def is_criticality_balanced(temperature, neutrons_emitted):
+    """Verify criticality is balanced.
+
+    :param temperature: temperature value (integer or float)
+    :param neutrons_emitted: number of neutrons emitted per second (integer or float)
+    :return:  boolean True if conditions met, False if not
+
+    A reactor is said to be critical if it satisfies the following conditions:
+    - The temperature is less than 800.
+    - The number of neutrons emitted per second is greater than 500.
+    - The product of temperature and neutrons emitted per second is less than 500000.
+    """
+
+    if temperature < 800 and neutrons_emitted > 500:
+        product_of_temperature_neutrons_emitted = temperature * neutrons_emitted
+        if product_of_temperature_neutrons_emitted < 500000:
+            return True
+
+    return False
+
+
+def reactor_efficiency(voltage, current, theoretical_max_power):
+    """Assess reactor efficiency zone.
+
+    :param voltage: voltage value (integer or float)
+    :param current: current value (integer or float)
+    :param theoretical_max_power: power that corresponds to a 100% efficiency (integer or float)
+    :return: str one of 'green', 'orange', 'red', or 'black'
+
+    Efficiency can be grouped into 4 bands:
+
+    1. green -> efficiency of 80% or more,
+    2. orange -> efficiency of less than 80% but at least 60%,
+    3. red -> efficiency below 60%, but still 30% or more,
+    4. black ->  less than 30% efficient.
+
+    The percentage value is calculated as
+    (generated power/ theoretical max power)*100
+    where generated power = voltage * current
+    """
+    percentage_of_value = ((voltage * current)/theoretical_max_power)*100
+    if percentage_of_value >= 80:
+        return 'green'
+
+    if 60 <= percentage_of_value < 80:
+        return 'orange'
+
+    if 30 <= percentage_of_value <60:
+        return 'red'
+
+    return 'black'
+
+
+def fail_safe(temperature, neutrons_produced_per_second, threshold):
+    """Assess and return status code for the reactor.
+
+    :param temperature: value of the temperature (integer or float)
+    :param neutrons_produced_per_second: neutron flux (integer or float)
+    :param threshold: threshold (integer or float)
+    :return: str one of: 'LOW', 'NORMAL', 'DANGER'
+
+    - `temperature * neutrons per second` < 90% of `threshold` == 'LOW'
+    - `temperature * neutrons per second` +/- 10% of `threshold` == 'NORMAL'
+    - `temperature * neutrons per second` is not in the above-stated ranges ==  'DANGER'
+    """
+
+    calculation = temperature * neutrons_produced_per_second
+    if calculation < threshold * 0.9:
+        return 'LOW'
+
+    if threshold * 0.9 < calculation < threshold * 1.1:
+        return 'NORMAL'
+
+    return 'DANGER'
