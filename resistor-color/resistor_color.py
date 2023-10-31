c851cd52d5678114b2e81551e21d8c3493d09be4
Mustafa YILDIRIM <mustafa.mym9@gmail.com>
2023-10-30 17:11:27 -0700

    solve resistor-color

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,8 @@
+MAP = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+def color_code(color):
+    return MAP.index(color)
+
+
+def colors():
+    return MAP
